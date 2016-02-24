d94c0be812108d12c77b7cc46fa844c1bc70432a
Mattis Erngren <hello@mattiserngren.com>
2016-02-24 21:23:00 +0100

    Add ajax support

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-var firstNamePart = "Dor";
-var secondNamePart = "nark";
+var nameList = "";
+var generatedName = "";
 
-jQuery("#generateButton").click(function() {
-  jQuery("div#generated-name").html(firstNamePart + secondNamePart);
-});
+// Get names and parse to JSON
+
+var jsonNames = new XMLHttpRequest();
+jsonNames.onreadystatechange = function() {
+  if (jsonNames.readyState == 4 && jsonNames.status == 200) {
+  nameList = JSON.parse(jsonNames.responseText);
+  }
+};
+jsonNames.open("GET", "http://www.lightly.se/code/osr-name-generator/js/names.json", false);
+jsonNames.send();
+
+// Simple print function for re-use
+function print (message, div){
+  var divId = document.getElementById(div);
+  divId.innerHTML = message;
+}
+
+// Random name generator
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomNameGenerator () {
+  var firstNum = parseInt(getRandomNumber (0, 6));
+  var secondNum = parseInt(getRandomNumber (0, 6));
+  return nameList.nameParts.firstNamePart[firstNum] + nameList.nameParts.secondNamePart[secondNum];
+}
+
+generatedName = randomNameGenerator();
+
+print (generatedName, "generated-name");
