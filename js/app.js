999087c26ef5fe341c792a61ffd5304e1ed8a209
Mattis Erngren <hello@mattiserngren.com>
2016-02-24 21:35:51 +0100

    Add click to generate new name with jQuery

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 var nameList = "";
 var generatedName = "";
 
-// Get names and parse to JSON
-
 var jsonNames = new XMLHttpRequest();
 jsonNames.onreadystatechange = function() {
   if (jsonNames.readyState == 4 && jsonNames.status == 200) {
@@ -12,24 +10,26 @@ jsonNames.onreadystatechange = function() {
 jsonNames.open("GET", "http://www.lightly.se/code/osr-name-generator/js/names.json", false);
 jsonNames.send();
 
-// Simple print function for re-use
 function print (message, div){
   var divId = document.getElementById(div);
   divId.innerHTML = message;
 }
 
-// Random name generator
-
-function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomNumber () {
+  return Math.random() * (nameList.nameParts.firstNamePart.length);
 }
 
 function randomNameGenerator () {
-  var firstNum = parseInt(getRandomNumber (0, 6));
-  var secondNum = parseInt(getRandomNumber (0, 6));
+  var firstNum = parseInt(getRandomNumber());
+  var secondNum = parseInt(getRandomNumber());
   return nameList.nameParts.firstNamePart[firstNum] + nameList.nameParts.secondNamePart[secondNum];
 }
 
 generatedName = randomNameGenerator();
 
-print (generatedName, "generated-name");
+print (generatedName + " ..?", "generated-name");
+
+jQuery("#generateNameButton").click(function (){
+  generatedName = randomNameGenerator();
+  print (generatedName + " ..?", "generated-name");
+});
